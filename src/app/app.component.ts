1ef987b4d87fde0609884ab4d5b3d9f0d96287bc
Yader Centeno <yader_centeno@yahoo.com>
2020-08-12 18:38:53 -0600

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Unidad2-Ejercicio1';
+  titulo = 'Unidad 2 - Ejercicio 1';
+  subtitulo = "Calculadora";
+
+  valor1: number;
+  valor2: number;
+  resultado: number;
+
+  constructor() {
+    this.resultado = 0;
+  }
+
+  sumar(): void {
+    this.resultado = +this.valor1 + +this.valor2;
+  }
+
+  restar(): void {
+    this.resultado = +this.valor1 - +this.valor2;
+  }
+
+  multiplicar(): void {
+    this.resultado = +this.valor1 * +this.valor2;
+  }
+
+  dividir(): void {
+    this.resultado = +this.valor1 / +this.valor2;
+  }
+
 }
